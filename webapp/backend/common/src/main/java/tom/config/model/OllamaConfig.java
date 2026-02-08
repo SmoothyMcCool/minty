455558497490528525d31a9a116a38c1e8293c98
Tom von Moos <tom.vonmoos@gmail.com>
2026-02-08 15:15:04 -0500

    Cleaner connection pooling to play nicer with ollama.

--- webapp/backend/common/src/main/java/tom/config/model/OllamaConfig.java
+++ webapp/backend/common/src/main/java/tom/config/model/OllamaConfig.java
@@ -6,5 +6,5 @@ import java.util.List;
 
 public record OllamaConfig(URI uri, String defaultModel, List<ChatModelConfig> chatModels,
 		String conversationNamingModel, String diagrammingModel, int chatMemoryDepth, int defaultTopK,
-		Duration apiTimeout, Duration asyncResponseTimeout, int maxRequests, EmbeddingConfig embedding) {
+		Duration apiConnectTimeout, Duration apiTimeout, Duration asyncResponseTimeout, EmbeddingConfig embedding) {
 }
