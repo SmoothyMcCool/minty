418e721631f92951a85672bb964cca2a58a84554
Tom von Moos <tom.vonmoos@gmail.com>
2026-03-03 19:38:11 -0500

    Task to turn TSV data into a list of packets.

---
+++ webapp/backend/plugin-library/src/main/java/tom/tasks/transform/spreadsheet/ExtractSpreadSheet.java
@@ -0,0 +1,164 @@
+package tom.tasks.transform.spreadsheet;
+
+import java.util.List;
+import java.util.Map;
+
+import tom.api.model.services.ServiceConsumer;
+import tom.api.services.PluginServices;
+import tom.api.task.MintyTask;
+import tom.api.task.OutputPort;
+import tom.api.task.Packet;
+import tom.api.task.TaskConfigSpec;
+import tom.api.task.TaskLogger;
+import tom.api.task.TaskSpec;
+import tom.api.task.annotation.RunnableTask;
+import tom.tasks.TaskGroup;
+import tom.tasks.noop.NullTaskConfig;
+
+@RunnableTask
+public class ExtractSpreadSheet implements MintyTask, ServiceConsumer {
+
+	private List<? extends OutputPort> outputs;
+
+	private TaskLogger logger;
+	private Packet input;
+	private PluginServices pluginServices;
+	private boolean failed;
+	private String error;
+	private Packet result;
+
+	public ExtractSpreadSheet() {
+		input = null;
+		outputs = null;
+		failed = false;
+		error = null;
+		result = null;
+	}
+
+	public ExtractSpreadSheet(TaskConfigSpec config) {
+		this();
+	}
+
+	@Override
+	public void setPluginServices(PluginServices pluginServices) {
+		this.pluginServices = pluginServices;
+	}
+
+	@Override
+	public Packet getResult() {
+		return result;
+	}
+
+	@Override
+	public String getError() {
+		return error;
+	}
+
+	@Override
+	public void run() {
+		Packet result = new Packet();
+		input.getText().stream().map(text -> TsvExtractor.parse(text)).flatMap(List::stream)
+				.forEach(packet -> result.getData().addAll(packet.getData()));
+		result.setId(input.getId());
+		outputs.get(0).write(result);
+	}
+
+	@Override
+	public boolean giveInput(int inputNum, Packet dataPacket) {
+		if (inputNum != 0) {
+			failed = true;
+			throw new RuntimeException(
+					"ExtractSpreadSheet: Workflow misconfiguration detect. ExtractSpreadSheet should only ever have exactly one input!");
+		}
+
+		input = dataPacket;
+
+		return true;
+	}
+
+	@Override
+	public void setOutputConnectors(List<? extends OutputPort> outputs) {
+		if (outputs.size() != 1) {
+			logger.warn(
+					"ExtractSpreadSheet: Workflow misconfiguration detect. ExtractSpreadSheet should only ever have exactly one output!");
+		}
+		this.outputs = outputs;
+	}
+
+	@Override
+	public boolean readyToRun() {
+		return input != null;
+	}
+
+	@Override
+	public TaskSpec getSpecification() {
+		return new TaskSpec() {
+
+			@Override
+			public String description() {
+				return "Transform a spreadsheet in TSV format into a sequence of Packets.";
+			}
+
+			@Override
+			public String expects() {
+				return "This task produces packets from the provided TSV input. There will be one packet per row. "
+						+ "The first row is used as a header row, defining the keys in the data section of the emitted Packets.";
+			}
+
+			@Override
+			public String produces() {
+				return "A list of packets in Data.";
+			}
+
+			@Override
+			public int numOutputs() {
+				return 1;
+			}
+
+			@Override
+			public int numInputs() {
+				return 1;
+			}
+
+			@Override
+			public TaskConfigSpec taskConfiguration() {
+				return new NullTaskConfig();
+			}
+
+			@Override
+			public TaskConfigSpec taskConfiguration(Map<String, Object> configuration) {
+				return new NullTaskConfig(configuration);
+			}
+
+			@Override
+			public String taskName() {
+				return "Read TSV Data";
+			}
+
+			@Override
+			public String group() {
+				return TaskGroup.TRANSFORM.toString();
+			}
+		};
+	}
+
+	@Override
+	public void inputTerminated(int i) {
+	}
+
+	@Override
+	public boolean failed() {
+		return failed;
+	}
+
+	@Override
+	public void setLogger(TaskLogger workflowLogger) {
+		this.logger = workflowLogger;
+	}
+
+	@Override
+	public String html() {
+		return "";
+	}
+
+}
